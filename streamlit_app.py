fdf6aae6089def8708c40adb4896981a92448138
ptams <104543527+ptams@users.noreply.github.com>
2022-05-08 13:26:44 -0400

    Update streamlit_app.py
    Breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Mom\'s new Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bluebery Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Eggs')
