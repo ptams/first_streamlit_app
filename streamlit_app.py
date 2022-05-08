e997ffcb97ec9e2a2585f61f38554c8f4cb45fac
ptams <104543527+ptams@users.noreply.github.com>
2022-05-08 13:39:08 -0400

    Update streamlit_app.py
    Added emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My Mom\'s new Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bluebery Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Eggs')
+streamlit.text('🥣 Omega 3 & Bluebery Oatmeal')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Eggs')
+streamlit.text('🥑🍞 Avacado Toast')
